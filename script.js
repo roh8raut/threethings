af9d4e5cdf02293dac8f815e5bda8182df9ace1d
Heather Christman <efl7a@yahoo.de>
2019-10-07 14:17:34 -0400

    add check animation to css

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let things= ["work", "exercise", "study"];
 let order = []
-while(order.length < 3){ 
+while(order.length < 3){
     let choice = Math.floor(Math.random() * things.length);
     if(!order.includes(choice)){
         order.push(choice);
@@ -33,32 +33,43 @@ function toggleComplete(el){
     if(el.id == "firstContainer" && !firstComplete){
         //user clicked first container, change the check to green and the text to strike-through
         first.style.setProperty("text-decoration", "line-through");
+        markAs(firstCheck, "complete");
         firstCheck.style.color = "green";
         firstComplete = true;
-    } 
+    }
     else if(el.id == "firstContainer" && firstComplete){
         first.style.setProperty("text-decoration", "none");
+        markAs(firstCheck, "incomplete");
         firstCheck.style.color = "grey";
         firstComplete = false;
     }
     else if(el.id == "secondContainer" && !secondComplete){
         second.style.setProperty("text-decoration", "line-through");
+        markAs(secondCheck, "complete");
         secondCheck.style.color = "green";
         secondComplete = true;
-    } 
+    }
     else if(el.id == "secondContainer" && secondComplete){
         second.style.setProperty("text-decoration", "none");
+        markAs(secondCheck, "incomplete");
         secondCheck.style.color = "grey";
         secondComplete = false;
-    } 
+    }
     else if(el.id == "thirdContainer" && !thirdComplete){
         third.style.setProperty("text-decoration", "line-through");
+        markAs(thirdCheck, "complete");
         thirdCheck.style.color = "green";
         thirdComplete = true;
     }
     else if(el.id == "thirdContainer" && thirdComplete){
         third.style.setProperty("text-decoration", "none");
+        markAs(thirdCheck, "incomplete");
         thirdCheck.style.color = "grey";
         thirdComplete = false;
     }
+}
+
+function markAs(check, status) {
+  console.log(check.className)
+  check.className = `fas fa-check ${status}`;
 }
